e0edca1b247481ac6bd7e1c14a4c402a23745f83
Akash Belekar <akash.belekar@mobifilia.com>
2023-07-21 16:30:21 +0530

    Check TAAuth

---
+++ demoAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoAuth
+//
+//  Created by Akash Belekar on 21/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
